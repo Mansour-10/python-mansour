5978df803955c26c703cf6c5a4abecf6ce22c806
Mansour-Gulzad <gulzad.mansour2018@gmal.com>
2023-07-17 19:29:59 +0500

    add new files

---
+++ modifyString.py
@@ -0,0 +1,22 @@
+a = "Hello, World!"
+print(a.upper())
+
+a = "My name is Mohammad Mansour Gulzad"
+print(a.lower())
+
+a = "Hello, World!"
+print(a.replace("r", "s"))
+
+
+a = " University of central Asia "
+print(a.split(" ")) 
+
+first = "Hello"
+second = "World"
+finall = first + second
+print(finall)
+
+a = "Hello"
+b = "World"
+
+print( a + " " + b)
---
+++ string_1.py
@@ -0,0 +1,10 @@
+name = "mohammad mansour"
+#       0123 counting starts from 0 in python 
+# slicing
+
+print(name[2:5])
+
+
+
+
+
