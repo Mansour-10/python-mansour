2211a33d8efbe3f797d7408c66f565a57d95f215
Mansour-Gulzad <gulzad.mansour2018@gmal.com>
2023-07-17 19:53:08 +0500

    all lessons is in

--- formatString.py
+++ formatString.py
@@ -1,6 +1,7 @@
 year = 2025
 totalStudents = 50000
 semester = "python"
+day = "even"
 
-finalResult = "in {} numbers of students were {}, class {}"
-print(finalResult.format(year, totalStudents, semester))
+finalResult = "in {} numbers of students were {}, class {} are {}"
+print(finalResult.format(year, totalStudents, semester, day))
