2ce61a11c418f4b8b8faf195befc3b21423368a4
Mansour-Gulzad <gulzad.mansour2018@gmal.com>
2023-07-31 19:32:29 +0500

    all lessons for 31/7/2023 user input and try except

---
+++ try_except.py
@@ -0,0 +1,6 @@
+x = 5
+
+try:
+  print(x)
+except:
+  print("An exception occurred")
---
+++ user_input.py
@@ -0,0 +1,27 @@
+username = input("What is your name?") 
+print("your name is " + username)
+
+
+
+# how to calculate the year of birth
+age = input("how old are you?")
+x = 2023 - int(age)
+
+print("you was born in " + str(x))
+
+
+# another way 
+ 
+find_year = input("How old are you ?")
+
+result = 2023 - int(find_year)
+
+print(result)
+
+# next example
+
+name = input("what's ur name ?")
+Lastname = input("what's ur last name ?")
+phone = input("what's ur phone number") 
+
+print("My Name is" + name + " My last name is " + Lastname + "My phone number is " + phone)
